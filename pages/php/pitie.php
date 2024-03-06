7524563f08184bf4809ff6fb2744b71ba93f10fb
Jordan LANSOY <jordanlansoypro@gmail.com>
2024-03-06 22:55:33 +0100

    corretion des oublis -> rajout du dossier uploads / changement de destination de l'index

---
+++ pages/php/pitie.php
@@ -0,0 +1,30 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "Le fichier est une image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "Le fichier n'est pas une image.";
+        $uploadOk = 0;
+    }
+}
+
+if ($uploadOk == 0) {
+    echo "Désolé, votre fichier n'a pas été téléchargé.";
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "Le fichier " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " a été téléchargé.";
+
+        header("Location: pitieAfter.php");
+        exit; 
+    } else {
+        echo "Désolé, une erreur s'est produite lors du téléchargement de votre fichier.";
+    }
+}
+?>
---
+++ pages/php/pitieAfter.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <meta charset="utf-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <title>Pokemon Go</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <link rel="stylesheet" type="text/css" media="screen" href="../../style/bootperso.css" />
+   <link rel="stylesheet" type="text/css" media="screen" href="../../style/style.css" />
+</head>
+
+<body>
+   <main class="centered">
+      <div class="row col-12 col-sm-9 col-md-6 col-lg-3 centered">
+         <div class="box">
+            <h1>NEXUM</h1>
+            <p>Plateforme de peer-learning</p>
+            <p>Epsi Lille</p>
+            <?php
+                echo "Le fichier " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " a été téléchargé.";
+            ?>
+         </div>
+      </div>
+   </main>
+</body>
+
+</html>
